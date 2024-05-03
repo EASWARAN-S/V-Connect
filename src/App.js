520c6e073cf7aa671561d3edcd1821555a055ec3
EASWARAN-S <seaswaran007@gmail.com>
2024-05-03 13:22:15 +0530

    Signed-off-by: EASWARAN-S <seaswaran007@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import * as React from 'react';
+import Drawer from '@mui/material/Drawer';
+import signup from './components/signup'
+
+function MyButton() {
+  return (
+    <signup />
+
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='Drawer' >
+        <button onClick={MyButton()}>signup
+        </button>
+      </div>
+      <div className='Drawer'>
+        <button onClick={MyButton()}>signup
+        </button>
+      </div>
+
+
     </div>
+
   );
 }
 
